ef224eebdc8b50e2946d4f8dde62be356b571de6
arcticmonkey07 <arcticmonkey07@gmail.com>
2019-09-23 01:09:32 +0300

    response 320 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,47 @@
 'use strict';
 
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  nav: false,
+  dots: true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+    0:{
+      items:1,
+      nav:false
+    }
+  }
+});
+
+$(document).ready(function() {
+  $('#description-photo').on('click', function(ev) {
+    var player = $("#description-video")[0];
+    player.src += "&autoplay=1";
+    $("#description-photo").hide();
+    $("#description-play").hide();
+    ev.preventDefault();
+  });
+
+// ---------------------------------
+
+  $('#show-all-photos').on('click', function(ev) {
+    ev.preventDefault();
+    $("#photos-container").slideToggle("fast");
+    $("#photos-container").css("display", "flex");
+  });
+
+// ---------------------------------
+
+  var header = $('.about__button');
+  function accordion() {
+    $(this).toggleClass('active');
+    $(this).prev().slideToggle('normal');
+  }
+  header.click(accordion);
+
+});
+
 (function () {
 
   let reserveButton = document.querySelector('.header-middle__button');
@@ -17,5 +59,5 @@
     modalReserve.classList.remove('modal-show-animate');
     modalReserve.classList.add('visually-hidden');
   });
-  
+
 })();
